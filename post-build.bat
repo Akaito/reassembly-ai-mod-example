3c54fee85187d56de51e64176892b4937e88fef9
Chris Barrett <chris@codesaru.com>
2017-10-29 20:05:18 -0700

    Post-build script to copy DLL out works.

--- post-build.bat
+++ post-build.bat
@@ -3,7 +3,8 @@ set MY_AI_OUTPUT="E:\Program Files (x86)\Steam\steamapps\common\Reassembly\mods\
 ::echo %MY_AI_OUTPUT%
 ::pause
 
-:: Copy AI DLL and PDB (for debugging) to your mod's dir
-copy /Y Release\*.dll %MY_AI_OUTPUT%
-copy /Y Release\*.pdb %MY_AI_OUTPUT%
+:: Copy AI DLL and PDB (for debugging) to your mod's AI dir.
+:: That "%~dp0" craziness just means "this .bat file's location".
+copy /Y %~dp0\Release\*.dll %MY_AI_OUTPUT%
+copy /Y %~dp0\Release\*.pdb %MY_AI_OUTPUT%
 pause
